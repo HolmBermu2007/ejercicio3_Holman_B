59ce692b52de1efc25bf3fcd010449413abc9b37
Admin <Admin@DESKTOP-VBJGGPH>
2023-05-05 10:25:11 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio3_Holman_B/Computadora.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3_Holman_B
+{
+    internal class Computadora
+    {
+        int disco;
+        int Esp_disp;
+        bool encendido= false;
+
+        public Computadora()
+        {
+
+        }
+        public Computadora(int disco, int esp_disp, bool encendido)
+        {
+            this.Disco = disco;
+            this.Esp_disp1 = esp_disp;
+            this.Encendido = encendido;
+        }
+
+        public int Disco { get => disco; set => disco = value; }
+        public int Esp_disp1 { get => Esp_disp; set => Esp_disp = value; }
+        public bool Encendido { get => encendido; set => encendido = value; }
+
+        public void encender()
+        {
+            Console.WriteLine("desea encender el equipo");
+            if(this.Encendido)
+            {
+                
+                Console.WriteLine("la computadora se encendio");
+              
+            }
+            else
+            {
+                this.Encendido = false;
+                Console.WriteLine("el dispositivo esta encendido");
+               
+            }
+        }
+        public void Apagar()
+        {
+            Console.WriteLine("precione N para apagar el equipo");
+            char m= char.Parse(Console.ReadLine());
+            if(m=='n'|| m=='n')
+            {
+                this.encendido=false;
+                Console.WriteLine("el dispositivo esta apagado......");
+               
+
+            }
+            else
+            {
+
+                this.Encendido=true;
+                Console.WriteLine("se esta encendiendo el equipo");
+                
+            }
+        }
+        public void añadirdatos(int datos)
+        {
+            if (this.encendido)
+            {
+
+
+                if (datos <= this.disco)
+                {
+                    this.Esp_disp = this.disco - datos;
+                    {
+                        Console.WriteLine(" la capacidad de disco ha disminuido: {0}", Esp_disp);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no hay espacio en el disco");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" el equipo esta apagado....");
+            }
+        }
+        public void Eliminardato(int dato)
+        {
+
+            if (this.encendido)
+            {
+
+
+                if (dato <= this.Esp_disp)
+                {
+                    this.disco = this.Esp_disp + dato;
+                    {
+                        Console.WriteLine(" se elimino el disco y la capacidad de disco ha aumentado: {0}", disco);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no se elimino el dato");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" el equipo esta apagado....");
+            }
+        }
+    }
+}
---
+++ ejercicio3_Holman_B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3_Holman_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computadora com = new Computadora();
+            com.encender();
+            
+            Console.WriteLine("De que tamaño es su disco: ");
+            com.Disco=int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el archivo a añadir: ");
+            int datos=int.Parse(Console.ReadLine());
+            com.añadirdatos(datos);
+            int capacidad;
+            Console.WriteLine("ingrese el dato a eliminar: ");
+            capacidad=int.Parse(Console.ReadLine());
+            com.Eliminardato(capacidad);
+            com.Apagar();
+           
+            Console.ReadKey();
+
+        }
+    }
+}
